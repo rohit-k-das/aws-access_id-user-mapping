0054a6386e328e7fab6ee5907e23380e0ed09d04
root-xcutioner <35469053+root-xcutioner@users.noreply.github.com>
2018-03-09 04:39:15 -0500

    Updated 9 March 2018

---
+++ access_id_to_user.py
@@ -0,0 +1,62 @@
+import boto3
+import os
+
+#Get user name for access id
+def access_keys(profile,accesskeyid):
+    session = boto3.session.Session(profile_name = profile)
+    client = session.client('iam')
+    userlist = client.list_users()
+    for user in userlist['Users']:
+        username = user['UserName']
+        keylist = client.list_access_keys(UserName=username)
+        for key in keylist['AccessKeyMetadata']:
+            key_ID = key['AccessKeyId']
+            if accesskeyid == key_ID:
+                print "Access id belongs to user " + key['UserName'] + " in profile " + profile
+                exit(0)
+
+#Get all AWS account profiles from aws credentials file
+def get_profiles(cred_file):
+    profiles = []
+    try:
+        with open(cred_file) as f:
+            for line in f.readlines():
+                if '[' in line:
+                    line = line.replace('[','').replace(']','').strip('\n')
+                    profiles.append(line)
+    except Exception,e:
+        print "Error:" +str(e)
+    return profiles
+
+#Get default home dir of user executing the script
+def get_home_dir():
+    current_user_id = os.getuid()
+    with  open('/etc/passwd') as passwd_file:
+        for line in passwd_file.readlines():
+            field = line.split(':')
+            if current_user_id == int(field[2]):
+                home_dir = field[5]
+    return home_dir
+
+def main():
+    home_dir = get_home_dir()
+    cred_file_path = home_dir + '/.aws/credentials'
+
+    #Checks if aws credential file exists and get all AWS account profiles
+    if os.path.exists(cred_file_path):
+        profiles = get_profiles(cred_file_path)
+    else:
+        cred_file_path = raw_input("Please enter credential files absolute path: ")
+        profiles = get_profiles(cred_file_path)
+
+    #Access ID to be searched
+    accesskeyid = raw_input("Enter Access ID: ")
+    accesskeyid = accesskeyid.strip('\n').strip('\r')
+    print "Scanning profiles " + str(profiles) + " for access id " + accesskeyid + ' ...\n'
+
+    for profile in profiles:
+        access_keys(profile,accesskeyid)
+
+if __name__ == '__main__':
+    main()
+
